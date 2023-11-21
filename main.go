91f6c4e82720f3eea9e0d35e98e30ce36f8649af
Amirul Zafrin <amirulzafrin98@gmail.com>
2023-11-21 20:18:14 +0800

    Add main

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strings"
+)
+
+func main() {
+	// 	Hello()
+	fmt.Println("Listening on port :3000")
+
+	// Create a new server
+	l, err := net.Listen("tcp", "127.0.0.1:3000")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Server running on : ", l.Addr())
+
+	// Listen for connections
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer conn.Close()
+
+	for {
+		resp := NewResp(conn)
+		value, err := resp.Read()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		if value.typ != "array" {
+			fmt.Println("Invalid request, expected array")
+			continue
+		}
+
+		if len(value.array) == 0 {
+			fmt.Println("Invalid request, expected array length > 0")
+			continue
+		}
+
+		command := strings.ToUpper(value.array[0].bulk)
+		args := value.array[1:]
+
+		writer := NewWriter(conn)
+
+		handler, ok := Handlers[command]
+		if !ok {
+			fmt.Println("Invalid command: ", command)
+			writer.Write(Value{typ: "string", str: ""})
+			continue
+		}
+
+		result := handler(args)
+		writer.Write(result)
+	}
+}
